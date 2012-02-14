aded7049202acc88f14b362a70e39861eed0be89
Ajey Gore <agore@thoughtworks.com>
2012-02-14 11:23:48 +0530

    Some cleanup of test code

--- test/go_figure/go_config_endpoint_test.rb
+++ test/go_figure/go_config_endpoint_test.rb
@@ -7,7 +7,7 @@ module GoFigure
     def test_should_fetch_xml_config_on_localhost
       md5 = '7455edff001e2f262beb7c13f10ff7cb'
       fetcher = GoConfigEndpoint.new(:host => 'example.com', :port => 1234)
-      fetcher.http_fetcher = fetcher_with_config_file('foo.xml', md5, 'example.com', 1234)
+      fetcher.http_fetcher = fetcher_with_config_file('foo.xml', md5, config_endpoint)
 
       config = fetcher.get
       assert_equal config_file('foo.xml'), config.original_xml
@@ -17,30 +17,25 @@ module GoFigure
     def test_should_post_back_new_config_xml_content_with_original_md5
       md5 = '7455edff001e2f262beb7c13f10ff7cb'
       fetcher = GoConfigEndpoint.new(:host => 'example.com', :port => 1234)
-      fetcher.http_fetcher = fetcher_with_config_file('foo.xml', md5, 'example.com', 1234)
+      fetcher.http_fetcher = fetcher_with_config_file('foo.xml', md5, config_endpoint)
 
       config = fetcher.get
       config.xml = "<new xml content/>"
 
-      fetcher.http_fetcher.register_content("OK",
-                                          "http://example.com:1234/go/api/admin/config.xml",
-                                          :post,
-                                          :md5 => config.original_md5,
-                                          :xmlFile => config.xml)
+      fetcher.http_fetcher.register_content("OK", config_endpoint, :post, :md5 => config.original_md5, :xmlFile => config.xml)
       fetcher.post(config)
-      assert fetcher.http_fetcher.invoked?("http://example.com:1234/go/api/admin/config.xml",
-                                          :post,
-                                          :md5 => config.original_md5,
-                                          :xmlFile => config.xml)
-
+      assert fetcher.http_fetcher.invoked?(config_endpoint, :post, :md5 => config.original_md5, :xmlFile => config.xml)
     end
 
-    def fetcher_with_config_file(file_name, md5, host = 'localhost', port = 8153)
+    def config_endpoint
+      "http://example.com:1234/go/api/admin/config.xml"
+    end
+
+    def fetcher_with_config_file(file_name, md5, url)
       fetcher = StubHttpFetcher.new
-      fetcher.register_content(config_file(file_name), "http://#{host}:#{port}/go/api/admin/config.xml", :get, 'X-CRUISE-CONFIG-MD5' => md5)
+      fetcher.register_content(config_file(file_name), config_endpoint, :get, 'X-CRUISE-CONFIG-MD5' => md5)
       fetcher
     end
-
 
     def config_file(file_name)
       File.read("test/fixtures/#{file_name}")
