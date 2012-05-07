0a86ccffed412cd69490aa7f420e0f35bc7e284d
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2012-05-07 14:52:48 +0530

    Add simplecov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,14 @@
+require 'rubygems'
+require 'bundler/setup'
 require 'simplecov'
 SimpleCov.start do
+  add_filter "/.bundle/"
   add_filter "/bundle/"
 end
 
+Bundler.require
 require 'test/unit'
+
 require 'go_figure/test'
 
 require 'go_figure'
