78a19511ffa590ef7709c0ffd5be6d690d087a1e
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2012-05-08 14:23:01 +0530

    Change the version string

--- lib/go_figure/version.rb
+++ lib/go_figure/version.rb
@@ -1,3 +1,9 @@
 module GoFigure
-  VERSION = "0.0.10"
+  module Version
+    MAJOR = 0
+    MINOR = 0
+    PATCH = 10
+    RELEASE = ENV['GO_PIPELINE_COUNTER']
+    VERSION = [MAJOR, MINOR, PATCH, RELEASE].compact.join('.')
+  end
 end
