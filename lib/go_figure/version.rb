87d5d2b6a4b3da21adedf3196a5e07cbaa6a6dd0
Shishir Das <shishir.das@gmail.com>
2012-05-24 15:01:02 +0530

    bumped up version

--- lib/go_figure/version.rb
+++ lib/go_figure/version.rb
@@ -2,7 +2,7 @@ module GoFigure
   module Version
     MAJOR = 0
     MINOR = 0
-    PATCH = 10
+    PATCH = 11
     RELEASE = ENV['GO_PIPELINE_COUNTER']
     VERSION = [MAJOR, MINOR, PATCH, RELEASE].compact.join('.')
   end
